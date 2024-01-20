962db5acbcb32ade100e213afc83463b89ea897d
utf <utf16+github@riseup.net>
2024-01-20 22:10:13 +0100

    refactor(Main): merge [tracker] and [enet] config into [master]

--- Source/Main.c
+++ Source/Main.c
@@ -16,20 +16,16 @@ int main(void)
     server_args args;
 
     toml_table_t* parsed;
-    toml_table_t* tracker_table;
-    toml_table_t* enet_table;
+    toml_table_t* master_table;
     TOMLH_READ_FROM_FILE(parsed, "config.toml");
 
-    /* [tracker] */
-    TOMLH_GET_TABLE(parsed, tracker_table, "tracker");
-    TOMLH_GET_INT(tracker_table, args.port, "port", DEFAULT_SERVER_PORT, 0);
-
-    /* [enet] */
-    TOMLH_GET_TABLE(parsed, enet_table, "enet");
-    TOMLH_GET_INT(enet_table, args.max_connections, "max_connections", 128, 1);
-    TOMLH_GET_INT(enet_table, args.max_bandwidth_in, "max_bandwidth_in", 0, 1);
-    TOMLH_GET_INT(enet_table, args.max_bandwidth_out, "max_bandwidth_out", 0, 1);
-    TOMLH_GET_INT(enet_table, args.channels, "channels", 2, 1);
+    /* [master] */
+    TOMLH_GET_TABLE(parsed, master_table, "master");
+    TOMLH_GET_INT(master_table, args.port, "port", DEFAULT_SERVER_PORT, 0);
+    TOMLH_GET_INT(master_table, args.max_connections, "max_connections", 128, 1);
+    TOMLH_GET_INT(master_table, args.max_bandwidth_in, "max_bandwidth_in", 0, 1);
+    TOMLH_GET_INT(master_table, args.max_bandwidth_out, "max_bandwidth_out", 0, 1);
+    TOMLH_GET_INT(master_table, args.channels, "channels", 2, 1);
 
     server_t server = {0};
 

86fcdffb9977d661a33e90a4c94d3ff90737e63a
utf <utf16+github@riseup.net>
2024-11-19 23:36:33 +0100

    fix(CountUpdate): verify packet length to be 1

--- Source/Server/Packets/CountUpdate.c
+++ Source/Server/Packets/CountUpdate.c
@@ -1,11 +1,17 @@
 #include <Server/Packets/CountUpdate.h>
 #include <Server/Structs/ClientStruct.h>
 #include <Util/DataStream.h>
+#include <Util/Log.h>
 #include <Util/Mem.h>
 
 count_update_pkt* parse_count_update_packet(client_t* client, stream_t* stream)
 {
     auto packet = make(count_update_pkt);
+
+    if (stream->length != 1) {
+        LOG_CLIENT_WARNING(client, "Invalid CountUpdate length");
+        return NULL;
+    }
 
     (void) client;
     packet->current_players = stream_read_u8(stream);

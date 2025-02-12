ee164b654803b4e98186ca6f2f07903967c8d7d5
utf <utf16+github@riseup.net>
2025-02-12 18:14:06 +0100

    test(ProtocolVersion): add test cases for `protocol_version_to_string`

--- Source/Tests/Utils/TestProtocolVersion.c
+++ Source/Tests/Utils/TestProtocolVersion.c
@@ -7,4 +7,9 @@ int main() {
     assert_false(protocol_version_is_valid(100));
     assert_true(protocol_version_is_valid(31));
     assert_true(protocol_version_is_valid(32));
+
+    assert_str_equals(protocol_version_to_string(VERSION_054), "0.54");
+    assert_str_equals(protocol_version_to_string(VERSION_060), "0.60");
+    assert_str_equals(protocol_version_to_string(VERSION_075), "0.75");
+    assert_str_equals(protocol_version_to_string(VERSION_076), "0.76");
 }
